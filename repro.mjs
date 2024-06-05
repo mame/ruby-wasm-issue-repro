52b0918a7fdf3f55884295f25027e5666da6efe4
Yusuke Endoh <mame@ruby-lang.org>
2024-06-06 01:22:36 +0900

    Simplify the repro

--- repro.mjs
+++ repro.mjs
@@ -2,9 +2,7 @@ import { RubyVM, consolePrinter } from "./node_modules/@ruby/wasm-wasi/dist/esm/
 import { WASI, File, OpenFile, PreopenDirectory } from "./node_modules/@bjorn3/browser_wasi_shim/dist/index.js";
 import fs from "fs";
 
-const fd = fs.openSync("/dev/stdin", "rs");
-
-const setupRubyWasm = async (image, out) => {
+const setupRubyWasm = async (image) => {
   const module = await WebAssembly.compile(image);
   const fds = [
     new OpenFile(new File([])),
@@ -20,7 +18,7 @@ const setupRubyWasm = async (image, out) => {
   };
   vm.addToImports(imports);
 
-  const printer = consolePrinter({ stdout: out, stderr: out });
+  const printer = consolePrinter();
   printer.addToImports(imports);
   const instance = await WebAssembly.instantiate(module, imports);
   await vm.setInstance(instance);
@@ -33,99 +31,21 @@ const setupRubyWasm = async (image, out) => {
   return vm;
 }
 
-const buffer = [];
-const waiters = [];
-process.stdin.on("readable", () => {
-    let chunk;
-    while (chunk = process.stdin.read()) {
-        for (let i = 0; i < chunk.length; i++) {
-            buffer.push(chunk[i]);
-        }
-    }
-    if (buffer.length >= 1) {
-        for (let i = 0; i < waiters.length; i++) {
-            waiters[i]();
-        }
-        waiters.length = 0;
-    }
-});
-
 const main = async () => {
-  const image = fs.readFileSync("./node_modules/@ruby/head-wasm-wasi/dist/ruby.debug+stdlib.wasm");
-  const vm = await setupRubyWasm(image, (data) => process.stdout.write(data));
+  const image = fs.readFileSync("./node_modules/@ruby/head-wasm-wasi/dist/ruby+stdlib.wasm");
+  const vm = await setupRubyWasm(image);
 
   vm.eval("-> js_funcs { JSFuncs = js_funcs }").call("call", vm.wrap({
-    getByte: async () => {
-        const buffer = Buffer.alloc(1);
-        fs.readSync(fd, buffer, 0, 1, null);
-        return buffer[0];
+    foo: async () => {
+        return "foo";
     },
   }));
   
   vm.evalAsync(`
-  require "js"
-
-  # Hack to ignore "require 'io/console'" and "require 'io/wait'"
-  Dir.mkdir("/tmp")
-  Dir.mkdir("/tmp/io")
-  File.write("/tmp/io/console.rb", "")
-  File.write("/tmp/io/wait.rb", "")
-  $LOAD_PATH.unshift("/tmp")
-  module Kernel
-    alias_method :require, :gem_original_require
-  end
-  
-  # io shim
-  class IO
-    alias getbyte_orig getbyte
-    def getbyte
-      if to_i == 0
-        c = JSFuncs[:getByte].apply().await
-        return c == JS::Null ? nil : c.to_i
-      end
-      getbyte_orig
-    end
-  
-    alias getc_orig getc
-    def getc
-      return getbyte&.chr if to_i == 0
-      getc_orig
-    end
-  
-    def gets
-      s = ""
-      while c = getc
-        s << c
-        break if c == "\n"
-      end
-      s
-    end
-  
-    def tty?
-      false
-    end
-  end
-  
-  require "irb"
-  
-  # Hack to avoid IO.open(1, "w")
-  module IRB
-    class StdioInputMethod < InputMethod
-      def initialize
-        @line_no = 0
-        @line = []
-        @stdin = IO.open(STDIN.to_i, :external_encoding => IRB.conf[:LC_MESSAGES].encoding, :internal_encoding => "-")
-        # original: @stdout = IO.open(STDOUT.to_i, 'w', :external_encoding => IRB.conf[:LC_MESSAGES].encoding, :internal_encoding => "-")
-        @stdout = STDOUT
-      end
-    end
-  end
-  
-  # Run irb
-  IRB.setup(nil, argv: ['--no-pager'])
-  GC.stress = true
-  IRB::Irb.new.run
-    `);
+    #TracePoint.new {|tp| p tp }.enable
+    GC.stress = true
+    JSFuncs[:foo].apply().await
+  `);
 };
 
 main();
